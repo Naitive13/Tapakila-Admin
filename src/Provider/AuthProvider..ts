bae66c2df67d91ffd30abc4866ca85298c06710d
Randy-creator <hei.randy.2@gmail.com>
2025-03-20 13:27:23 -0700

    fix(authProvider): refactor the authProvider to handle login from the backend data

--- src/Provider/AuthProvider..ts
+++ src/Provider/AuthProvider..ts
@@ -1,35 +1,39 @@
 import { AuthProvider, QueryFunctionContext } from "react-admin";
+import { BASE_URL } from "../Constant";
 
 const authProvider: AuthProvider = {
     login: async (params: any): Promise<{ redirectTo?: string | boolean } | void> => {
+        console.log("Received params in login:", params);
         const { username, password } = params;
 
         try {
-            const response = await fetch('https://dummyjson.com/auth/login', {
+            if (!username || !password) {
+                throw new Error("Email and password are required.");
+            }
+
+            const response = await fetch(`${BASE_URL}/auth/login`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                    username: 'emilys',
-                    password: 'emilyspass',
-                    expiresInMins: 30,
-                }),
+                body: JSON.stringify({ email: username, password }),
             });
 
             if (!response.ok) {
-                throw new Error("Invalid login !");
+                const errorData = await response.json();
+                console.log("Error data:", errorData); 
+                throw new Error(errorData.message || "Invalid email or password!");
             }
 
             const data = await response.json();
             console.log("Login success:", data);
-
             localStorage.setItem("accessToken", data.token);
+            return { redirectTo: "/" };
 
-            return Promise.resolve({ redirectTo: "/" });
         } catch (error) {
             console.error("Login failed:", error);
-            return Promise.reject(error);
+            return Promise.reject(error.message);
         }
     },
+
 
     logout: async (): Promise<void> => {
         localStorage.removeItem("accessToken");
