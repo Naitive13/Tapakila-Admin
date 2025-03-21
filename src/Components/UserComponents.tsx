e5359f21ebcef8fac4514f640d8c47cf5f7dde55
Randy-creator <hei.randy.2@gmail.com>
2025-03-20 23:13:49 -0700

    feat: add components to show the users list

---
+++ src/Components/UserComponents.tsx
@@ -0,0 +1,15 @@
+import { List, Datagrid, TextField, RichTextField } from "react-admin";
+
+export const UserList = () => {
+  return (
+    <List>
+      <Datagrid>
+        {/* <TextField source="userId" /> */}
+        <TextField source="userName" />
+        <TextField source="email" />
+        <RichTextField source="type" label="type" />
+        <RichTextField source="creationDate" label="creation date" />
+      </Datagrid>
+    </List>
+  );
+};
