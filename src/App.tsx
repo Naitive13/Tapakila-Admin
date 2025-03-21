59b77b1679f723c827be2c399c2e5cc1ee831ae2
Randy-creator <hei.randy.2@gmail.com>
2025-03-20 23:14:36 -0700

    feat: add dataProvider and Ressources on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Admin, Resource } from "react-admin";
 import authProvider from "./Provider/AuthProvider.";
+import { UserDataProvider } from "./Provider/UserDataProvider";
+import { UserList } from "./Components/UserComponents";
 
 function App() {
   return (
     <>
-      <Admin authProvider={authProvider}>
-        <Resource name="post"></Resource>
+      <Admin authProvider={authProvider} dataProvider={UserDataProvider}>
+        <Resource name="User" list={UserList}></Resource>
       </Admin>
     </>
   );
