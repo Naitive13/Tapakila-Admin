321be349083f2dab17123316487bd08744a76489
Randy-creator <hei.randy.2@gmail.com>
2025-04-03 01:24:42 +0300

    feat: add getOne method on reservation's data provider

--- src/Provider/ReservationDataProvider.ts
+++ src/Provider/ReservationDataProvider.ts
@@ -1,9 +1,9 @@
-import { DataProvider, GetListParams, GetListResult, QueryFunctionContext, RaRecord } from "react-admin";
+import { CreateParams, CreateResult, DataProvider, DeleteManyParams, DeleteManyResult, DeleteParams, DeleteResult, GetListParams, GetListResult, GetManyParams, GetManyReferenceParams, GetManyReferenceResult, GetManyResult, GetOneParams, GetOneResult, Identifier, QueryFunctionContext, RaRecord, UpdateManyParams, UpdateManyResult, UpdateParams, UpdateResult } from "react-admin";
 import { BASE_URL } from "../Constant";
 
 export const ReservationDataProvider: DataProvider = {
     getList: async function <RecordType extends RaRecord = any>(resource: string, params: GetListParams & QueryFunctionContext): Promise<GetListResult<RecordType>> {
-        const response = await fetch(`${BASE_URL}/reservations/all`, { method: "GET" })
+        const response = await fetch(`${BASE_URL}/reservations/all`, { method: "GET" });
 
         if (!response.ok) {
             throw new Error('Network response was not ok');
@@ -19,6 +19,21 @@ export const ReservationDataProvider: DataProvider = {
             total: jsonData.length,
         };
 
+        return result;
+    },
+    getOne: async function <RecordType extends RaRecord = any>(resource: string, params: GetOneParams<RecordType> & QueryFunctionContext): Promise<GetOneResult<RecordType>> {
+        const { id } = params;
+        const response = await fetch(`${BASE_URL}/reservations/${id}`, { method: "GET" });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok !');
+        }
+
+        const jsonData = await response.json();
+
+        const result: GetOneResult = {
+            data: { ...jsonData, id: jsonData.reservationID },
+        };
         return result;
     },
 }
