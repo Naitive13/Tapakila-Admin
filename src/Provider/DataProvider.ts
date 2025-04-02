f1f38409def138d8e8b20380b31bc698ad540bf1
Randy-creator <hei.randy.2@gmail.com>
2025-04-03 00:26:29 +0300

    feat: create new file.ts to regroup all dataprovider

---
+++ src/Provider/DataProvider.ts
@@ -0,0 +1,47 @@
+import { DataProvider, RaRecord, GetListParams, QueryFunctionContext, GetListResult, GetOneParams, GetOneResult, UpdateParams, UpdateResult, Identifier, CreateParams, CreateResult } from "react-admin";
+import { UserDataProvider } from "./UserDataProvider";
+
+const getDataProvider = (resource: string) => {
+    switch (resource) {
+        case "User":
+            return UserDataProvider;
+
+
+        default:
+            throw new Error(resource);
+    }
+};
+export const dataProvider: DataProvider = {
+    getList: async function <RecordType extends RaRecord = any>(
+        resource: string,
+        params: GetListParams & QueryFunctionContext,
+    ): Promise<GetListResult<RecordType>> {
+        const currentDataProvider = getDataProvider(resource);
+        return currentDataProvider.getList(params);
+    },
+    getOne: async function <RecordType extends RaRecord = any>(
+        resource: string,
+        params: GetOneParams<RecordType> & QueryFunctionContext,
+    ): Promise<GetOneResult<RecordType>> {
+        const currentDataProvider = getDataProvider(resource);
+        return currentDataProvider.getOne(params);
+    },
+
+    update: async function <RecordType extends RaRecord = any>(
+        resource: string,
+        params: UpdateParams,
+    ): Promise<UpdateResult<RecordType>> {
+        const currentDataProvider = getDataProvider(resource);
+        return currentDataProvider.update(params);
+    },
+    create: async function <
+        RecordType extends Omit<RaRecord, "id"> = any,
+        ResultRecordType extends RaRecord = RecordType & { id: Identifier },
+    >(
+        resource: string,
+        params: CreateParams,
+    ): Promise<CreateResult<ResultRecordType>> {
+        const currentDataProvider = getDataProvider(resource);
+        return currentDataProvider.create(params);
+    },
+};
