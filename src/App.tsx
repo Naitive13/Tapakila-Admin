1182b1bf0f725a6f9cd480a5043a2e223e117da7
Randy-creator <hei.randy.2@gmail.com>
2025-04-03 00:27:22 +0300

    fix: change UserDataProvider to global dataProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Admin, Resource } from "react-admin";
 import { authProvider } from "./Provider/AuthProvider.";
-import { UserDataProvider } from "./Provider/UserDataProvider";
+import { dataProvider } from "./Provider/DataProvider";
 import { UserList, UserShow } from "./Components/UserComponents";
 
 function App() {
   return (
     <>
-      <Admin authProvider={authProvider} dataProvider={UserDataProvider}>
-        <Resource name="User" list={UserList} show={UserShow} ></Resource>
+      <Admin authProvider={authProvider} dataProvider={dataProvider}>
+        <Resource name="User" list={UserList} show={UserShow}></Resource>
       </Admin>
     </>
   );
