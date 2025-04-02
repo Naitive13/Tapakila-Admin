f2b5f7cc8522f36a9ad49db71bb5485d833aa764
Randy-creator <hei.randy.2@gmail.com>
2025-04-03 01:38:51 +0300

    feat: create Ticket data provider and add getList and getOne method

---
+++ src/Provider/TicketDataProvider.ts
@@ -0,0 +1,39 @@
+import { CreateParams, CreateResult, DataProvider, DeleteManyParams, DeleteManyResult, DeleteParams, DeleteResult, GetListParams, GetListResult, GetManyParams, GetManyReferenceParams, GetManyReferenceResult, GetManyResult, GetOneParams, GetOneResult, Identifier, QueryFunctionContext, RaRecord, UpdateManyParams, UpdateManyResult, UpdateParams, UpdateResult } from "react-admin";
+import { BASE_URL } from "../Constant";
+
+export const TicketDataProvider: DataProvider = {
+    getList: async function <RecordType extends RaRecord = any>(resource: string, params: GetListParams & QueryFunctionContext): Promise<GetListResult<RecordType>> {
+        const response = await fetch(`${BASE_URL}/tickets`, { method: "GET" });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const jsonData = await response.json();
+        for (let index = 0; index < jsonData.length; index++) {
+            const element = jsonData[index];
+            jsonData[index] = { ...element, id: index };
+        }
+        const result: GetListResult = {
+            data: jsonData.map(reservation => ({ ...reservation, id: reservation.reservationID })),
+            total: jsonData.length,
+        };
+
+        return result;
+    },
+    getOne: async function <RecordType extends RaRecord = any>(resource: string, params: GetOneParams<RecordType> & QueryFunctionContext): Promise<GetOneResult<RecordType>> {
+        const { id } = params;
+        const response = await fetch(`${BASE_URL}/tickets/${id}`, { method: "GET" });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok !');
+        }
+
+        const jsonData = await response.json();
+
+        const result: GetOneResult = {
+            data: { ...jsonData, id: jsonData.ticketID },
+        };
+        return result;
+    },
+}
