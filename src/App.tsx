52387fc8b5241fbb03f3f6d0249af10b199359de
Randy-creator <hei.randy.2@gmail.com>
2025-04-03 14:28:35 +0300

    feat: add event data provider in the switch case handler

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,27 @@ import { UserList } from "./Components/user/UserList";
 import { UserShow } from "./Components/user/UserShow";
 import { ReservationList } from "./Components/reservation/ReservationList";
 import { ReservationShow } from "./Components/reservation/ReservationShow";
+import { UserCreate } from "./Components/user/UserCreate";
+import { EventList } from "./Components/event/EventList";
+import { EventShow } from "./Components/event/EventShow";
 
 function App() {
   return (
     <>
       <Admin authProvider={authProvider} dataProvider={dataProvider}>
-        <Resource name="User" list={UserList} show={UserShow} create={UserCreate}></Resource>
+        <Resource
+          name="User"
+          list={UserList}
+          show={UserShow}
+          create={UserCreate}
+        ></Resource>
         <Resource
           name="Reservation"
           list={ReservationList}
           show={ReservationShow}
         ></Resource>
         <Resource name="Ticket" list={TicketList} show={TicketShow}></Resource>
+        <Resource name="Event" list={EventList} show={EventShow}></Resource>
       </Admin>
     </>
   );
