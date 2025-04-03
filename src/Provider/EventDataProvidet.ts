cb253ab86734451324616ca20d8ae752e1253bc3
Randy-creator <hei.randy.2@gmail.com>
2025-04-03 16:26:44 +0300

    feat: add data provider for event

--- src/Provider/EventDataProvidet.ts
+++ src/Provider/EventDataProvidet.ts
@@ -1,4 +1,4 @@
-import { DataProvider } from "react-admin";
+import { CreateParams, CreateResult, DataProvider, DeleteManyParams, DeleteManyResult, DeleteParams, DeleteResult, GetManyParams, GetManyReferenceParams, GetManyReferenceResult, GetManyResult, Identifier, QueryFunctionContext, RaRecord, UpdateManyParams, UpdateManyResult, UpdateParams, UpdateResult } from "react-admin";
 import { BASE_URL } from "../Constant";
 
 export const EventDataProvider: DataProvider = {
@@ -24,7 +24,7 @@ export const EventDataProvider: DataProvider = {
 
     getOne: async function <RecordType extends RaRecord = any>(resource: string, params: GetOneParams<RecordType> & QueryFunctionContext): Promise<GetOneResult<RecordType>> {
         const { id } = params;
-        const response = await fetch(`${BASE_URL}/event/${id}`, { method: "GET" });
+        const response = await fetch(`${BASE_URL}/events/${id}`, { method: "GET" });
 
         if (!response.ok) {
             throw new Error('Network response was not ok !');
@@ -35,6 +35,34 @@ export const EventDataProvider: DataProvider = {
         const result: GetOneResult = {
             data: { ...jsonData, id: jsonData.eventId },
         };
+        return result;
+    },
+    create: async function <RecordType extends Omit<RaRecord, "id"> = any, ResultRecordType extends RaRecord = RecordType & { id: Identifier; }>(
+        resource: string,
+        params: CreateParams
+    ): Promise<CreateResult<ResultRecordType>> {
+        const { data } = params;
+        console.log(data);
+        
+
+        const createEvent = await fetch(`${BASE_URL}/events`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+
+        if (!createEvent.ok) {
+            throw new Error('Failed to create event');
+        }
+
+        const resultData = await createEvent.json();
+
+        const result: CreateResult<ResultRecordType> = {
+            data: { ...resultData, id: resultData.eventId },
+        };
+
         return result;
     },
 }
